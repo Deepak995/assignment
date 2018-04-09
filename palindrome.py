c7eff9c30e1fc622ae41fbb600c610df13b57790
Deepak <37833236+Deepak995@users.noreply.github.com>
2018-04-09 17:58:05 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,32 +1,29 @@
 class palindrome:
     
-    def rev(self,s):
+    def rev(self,s):# this fun will remove the special charecter and reverse the given string
         r=""
         p=len(s)-1
         while p>=0:
-            """r=r+s[p]
             
-            p-=1"""
             if (s[p]>='a' and s[p]<='z') or(s[p]>='A' and s[p]<='Z') or(s[p]>='0' and s[p]<='9'):
                 r=r+s[p]
             
             p-=1
         return r
         
-    def rem(self,s):
+    def rem(self,s): # this fun will remove the special cherecter 
         v=""
         p=len(s)-1
         i=0
         while i<=p:
-            """v=v+s[i]
-            i+=1"""
+            
             
             if (s[i]>='a' and s[i]<='z') or(s[i]>='A' and s[i]<='Z')or(s[i]>='0' and s[i]<='9'):
                 v=v+s[i]
             i+=1
         return v
         
-    def com(self,a,b):
+    def com(self,a,b): # this fun will compare the reverse string and straigth string
         if (a.lower()==b.lower()):
             print("the string is palindrom")
         else: 
