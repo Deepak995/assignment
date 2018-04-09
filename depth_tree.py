8c37110102fad9d31904f153731774de3b8ad0d1
Deepak <37833236+Deepak995@users.noreply.github.com>
2018-04-09 10:50:24 +0530

    Add files via upload

---
+++ depth_tree.py
@@ -0,0 +1,33 @@
+class tree:
+ 
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+ 
+
+def maxDepth(node):
+    if node is None:
+        return 0 ; 
+ 
+    else :
+ 
+       
+        lDepth = maxDepth(node.left)
+        rDepth = maxDepth(node.right)
+ 
+        
+        if (lDepth > rDepth):
+            return lDepth+1
+        else:
+            return rDepth+1
+ 
+ 
+# Driver program to test above function
+root = tree(1)
+root.left = tree(2)
+root.right = tree(3)
+root.left.left = tree(4)
+root.left.right = tree(5)
+
+print ("max depth" ,(maxDepth(root))) 
