0f26973ca89d3a96f7dab8f679568c355e34426f
Deepak <37833236+Deepak995@users.noreply.github.com>
2018-04-10 11:12:53 +0530

    Update palindrome_file.py

--- palindrome_file.py
+++ palindrome_file.py
@@ -4,9 +4,7 @@ class palindrome:
         r=""
         p=len(s)-1
         while p>=0:
-            """r=r+s[p]
             
-            p-=1"""
             if (s[p]>='a' and s[p]<='z') or(s[p]>='A' and s[p]<='Z') or(s[p]>='0' and s[p]<='9'):
                 r=r+s[p]
             
@@ -18,8 +16,7 @@ class palindrome:
         p=len(s)-1
         i=0
         while i<=p:
-            """v=v+s[i]
-            i+=1"""
+            
             
             if (s[i]>='a' and s[i]<='z') or(s[i]>='A' and s[i]<='Z')or(s[i]>='0' and s[i]<='9'):
                 v=v+s[i]
